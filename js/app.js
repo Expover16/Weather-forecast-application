d6bc93486d96ea11660e28509fcf37625e723d5d
Expover16 <132396461+Expover16@users.noreply.github.com>
2024-01-15 12:15:17 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,69 @@
+const form = document.querySelector('[data-app="inputData"')
+const cityName = document.querySelector('[data-app="cityname"]')
+const localTemperature = document.querySelector('[data-app="temperature"]')
+const day_Image = document.querySelector('[data-app="time"]')
+const display_Weather = document.querySelector('[data-app="display_Weather"]')
+const display_Weather_Icon = document.querySelector('[data-app="weatherIcon"]')
+const displayInfoCard = document.querySelector('[data-app="infoCard"]')
+
+const storeDataCity = response => {
+    const objData = JSON.stringify(response)
+    localStorage.setItem('cityData', objData)
+    const getCityData = localStorage.getItem('cityData')
+    return getCityData
+}
+
+const storeDataConditions = response => {
+    const objData = JSON.stringify(response)
+    localStorage.setItem('cityConditions', objData)
+    const getCityConditions = localStorage.getItem('cityConditions')
+    return getCityConditions
+}
+
+const requests_Methods = {
+
+    showInfoCity(
+        LocalizedName,
+        WeatherText,
+        WeatherIcon,
+        Temperature,
+        IsDayTime) {
+        cityName.textContent = LocalizedName
+        display_Weather.textContent = WeatherText
+        display_Weather_Icon.innerHTML = `<img data-app="icon-ref" src="./icons/${WeatherIcon}.svg" class="icon-Ref "></img>`
+        localTemperature.textContent = Temperature.Metric.Value
+        IsDayTime ? day_Image.src = "./src/day.svg" : day_Image.src = "./src/night.svg"
+    },
+
+    async infoCityData(inputData) {
+        const responseCity = await getInfoCityWeather(inputData)
+        const [{ Key, LocalizedName }] = JSON.parse(storeDataCity(responseCity))
+
+        const responseConditions = await fetchURL(urlConditions(Key))
+        const [{ IsDayTime, WeatherText, WeatherIcon, Temperature }] = JSON.parse(storeDataConditions(responseConditions))
+
+        this.showInfoCity(LocalizedName, WeatherText, WeatherIcon, Temperature, IsDayTime)
+    }
+}
+
+function factory_Request_APP() {
+    let new_obj = Object.create(requests_Methods)
+    return new_obj
+}
+
+const enter_City_Name = factory_Request_APP()
+
+form.addEventListener("submit", event => {
+    event.preventDefault()
+    const inputData = event.target.inputCity.value.trim()
+
+    enter_City_Name.infoCityData(inputData)
+
+    localStorage.setItem('lastCity', inputData)
+
+    form.reset()
+})
+
+const lastCity = localStorage.getItem('lastCity')
+
+lastCity ? enter_City_Name.infoCityData(lastCity) : null
---
+++ js/weather.js
@@ -0,0 +1,24 @@
+const myAPIKey = 'bvsFtJOb7CoBi7g4zEOH05RfRIz1vAAA'
+
+const urlCity = inputData => `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${myAPIKey}&q=${inputData}`
+
+const urlConditions = keyCode => `http://dataservice.accuweather.com/currentconditions/v1/${keyCode}?apikey=${myAPIKey}&Language=en-us`;
+
+const fetchURL = async url => {
+    try {
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error('IHHH...Deu Pau...REFAÇA!!!')
+        }
+
+        const objPromise = await response.json()
+        return objPromise
+
+    } catch (error) { alert(error.message) }
+}
+
+const getInfoCityWeather = async inputData => {
+    const response = await fetchURL(urlCity(inputData))
+    return response
+}
